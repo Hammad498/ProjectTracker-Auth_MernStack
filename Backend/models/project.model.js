560959fbabd73fda5b4a0198f2cec7e8420f590c
Hammad498 <hammadabrar498@gmail.com>
2025-06-12 17:56:04 +0500

    multer & projectSchema setUp

---
+++ Backend/models/project.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const projectSchema=new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: [3, "Name must be at least 3 characters long"],
+      },
+      description: {
+        type: String,
+        required: true,
+        minlength: [3, "Description must be at least 3 characters long"],
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      link: {
+        type: String,
+        required: true,
+      },
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      updatedAt: {
+        type: Date,
+        default: Date.now,
+      },
+},{timestamps:true});
+
+
+const projectModel=mongoose.model("Project",projectSchema);
+
+export default projectModel
--- Backend/routes/project.routes.js
+++ Backend/routes/project.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/authenticate.middleware.js";
 import { createProject,getProject } from "../controllers/project.controller.js";
+import {upload} from '../middlewares/multer.middleware.js'
 
 
 const router=Router();
 
 
-router.post('/projects',authenticate,createProject);
+router.post('/projects',authenticate,upload.single('image'),createProject);
 
 router.get('/projects',authenticate,getProject);
 

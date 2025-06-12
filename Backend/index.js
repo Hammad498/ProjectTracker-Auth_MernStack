1d52bf5077f43d141443cea248ae601a42f9ed7a
Hammad498 <hammadabrar498@gmail.com>
2025-06-12 17:46:45 +0500

    error removed

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRoutes from './routes/auth.routes.js'
+import projectRoutes from './routes/project.routes.js'
 
 connectDB();
 
@@ -11,6 +12,7 @@ dotenv.config();
 const app=express();
 app.use(express.json());
 app.use('/api/auth',userRoutes);
+app.use('/api',projectRoutes);
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
